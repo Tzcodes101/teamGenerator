73a2aaf544e9581cfff866b9df3e543f0a61cce6
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-10-09 11:08:48 -0400

    manager class add

--- 02-Homework/Develop/lib/Manager.js
+++ 02-Homework/Develop/lib/Manager.js
@@ -2,3 +2,23 @@
 //manager will extend employee class
 //manager will also have office number property
 //engineer witll have getRole and get office number methods
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+}
+
+module.exports = Manager;
