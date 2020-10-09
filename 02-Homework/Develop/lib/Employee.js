3cfe317818f0d102d0c6aa811580da885961b287
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-10-09 11:07:06 -0400

    employee class addition

--- 02-Homework/Develop/lib/Employee.js
+++ 02-Homework/Develop/lib/Employee.js
@@ -2,3 +2,29 @@
 //employee will have name, id, and email properties
 //employee will have getName, getID, getEmail, and getRole methods
 //module will be exported
+class Employee {
+
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
