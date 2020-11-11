d26971a8331c969496bcfdc3ca89f218e166a4ae
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-11 07:46:27 -0500

    app.js logic and createTeam function addition

--- Develop/app.js
+++ Develop/app.js
@@ -13,3 +13,41 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+//create manager add name (validate is not empty), id(validate has numbers), email(validate has email characters), officenumber(validate has numbers),
+
+//create intern add name (validate has letters), id(validate is number), email(validate has email characters), school (validate has letters)
+
+//create engineer, add name (validate has letters), id(validate is number), email(validate has email characters), github name(validate has letters),
+
+//renderTeam
+
+//give user options to build their team
+function createTeam() {
+    console.log("Create team called");
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Would you like to build your team?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },
+    ]).then(answer => {
+        switch(answer.choice) {
+            case "Yes":
+                addManager();
+                break;
+            case "No":
+                break;
+            default:
+                addManager();
+        }
+    });
+
+}
+
+createTeam();
