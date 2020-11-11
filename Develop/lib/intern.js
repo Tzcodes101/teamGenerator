e757471694b45359c516bd9834f92ecc2110aecb
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 22:15:54 -0500

    intern class pseudocode

--- Develop/lib/intern.js
+++ Develop/lib/intern.js
@@ -1,24 +1,5 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-//intern will extend employee class
-//intern will also have school name property
-//intern witll have getRole and get school methods
-const Employee = require("./Employee");
+//create intern class, also has school
 
-class Intern extends Employee {
+//get school
 
-    constructor(name, id, email, school) {
-        super(name, id, email);
-        this.school = school;
-    }
-
-    getRole() {
-        return "Intern";
-    }
-
-    getSchool() {
-        return this.school;
-    }
-
-}
-
-module.exports = Intern;
+//getRole returns intern
