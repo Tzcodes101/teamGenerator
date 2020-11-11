b1c452b7abe0e9c0a69ff8ba92f64a0fa8e49c02
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 22:20:39 -0500

    employee test pseudocode

---
+++ Develop/tests/Employee.test.js
@@ -0,0 +1,19 @@
+//create a test that will check for:
+
+
+//if employee is an object
+
+//if you can set the name
+
+//can set the ID
+
+//can set emp email
+
+//can get name with proper function
+
+//can get id with proper function
+
+//can get email with proper function
+
+//can return the proper role 
+
