68381a65690d93488d7aed742560e6f618436c96
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 23:37:23 -0500

    intern class code addition

--- Develop/lib/intern.js
+++ Develop/lib/intern.js
@@ -3,3 +3,39 @@
 //get school
 
 //getRole returns intern
+class Intern {
+    constructor(name, id, email, school) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.school = school;
+    }
+
+    //be able to:
+
+    //get name
+    getName() {
+        return this.name;
+    }
+
+    //get id
+    getId() {
+        return this.id;
+    }
+
+    //get email
+    getEmail() {
+        return this.email;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    //get role (returns employee)
+    getRole() {
+        return "Intern";
+    }
+}
+
+module.exports = Intern;
