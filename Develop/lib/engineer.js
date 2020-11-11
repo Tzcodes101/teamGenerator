dc21dc60deb87a66a11e04a04d7e35df8e201d52
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 23:24:52 -0500

    engineer class code addition

--- Develop/lib/engineer.js
+++ Develop/lib/engineer.js
@@ -4,3 +4,35 @@
 
 //getRole returns engineer
 
+//create employee class with name, id, email
+class Engineer {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    //be able to:
+
+    //get name
+    getName() {
+        return this.name;
+    }
+
+    //get id
+    getId() {
+        return this.id;
+    }
+
+    //get email
+    getEmail() {
+        return this.email;
+    }
+
+    //get role (returns employee)
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.exports = Engineer;
