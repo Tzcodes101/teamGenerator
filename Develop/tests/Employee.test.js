b2447a4fc97c00eb1dad1aed219c2e1e9dc134fc
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 23:23:26 -0500

    finish employee test code

--- Develop/tests/Employee.test.js
+++ Develop/tests/Employee.test.js
@@ -29,18 +29,33 @@ test("Can set employee email", () => {
     const testEmail = "hi@hi.com";
     const e = new Employee("Rose", 30, testEmail);
     expect(e.email).toBe(testEmail);
-})
+});
 
 //can get name with proper function
 test("getName function returns name", () => {
     const testName = "Rose";
     const e = new Employee(testName);
     expect(e.getName()).toBe(testName);
-})
+});
 
 //can get id with proper function
+test("getId function returns id", () => {
+    const testId = 6;
+    const e = new Employee("Rose", testId);
+    expect(e.getId()).toBe(testId);
+});
 
 //can get email with proper function
+test("getEmail function returns email", () => {
+    const testEmail = "Hi@hi.com";
+    const e = new Employee("Rose", 8, testEmail);
+    expect(e.getEmail()).toBe(testEmail);
+});
 
 //can return the proper role 
+test("getRole function returns employee", () => {
+    const testRole = "Employee";
+    const e = new Employee ("Rose", 5, "hi@hi2.com");
+    expect(e.getRole()).toBe(testRole);
+});
 
