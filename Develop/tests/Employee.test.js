7dc6ce586ca437d843394f257db08d4e3cab8d7e
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 23:02:42 -0500

    employee name test code addition

--- Develop/tests/Employee.test.js
+++ Develop/tests/Employee.test.js
@@ -1,16 +1,21 @@
 //require Employee from the proper path
 const { TestScheduler } = require("jest");
-const Employee = require("../libEmployee");
+const Employee = require("../lib/Employee");
 
 //create a test that will check for:
 
 //if employee is an object
 test("Employee is an object", function() {
-    var employee = new Employee();
-    expect(typeof(employee).toBe("object"));
+    var e = new Employee();
+    expect(typeof(e)).toBe("object");
 })
 
 //if you can set the name
+test("Can set employee name", function () {
+    var name = "Rose";
+    var e = new Employee(name);
+    expect(e).toBe(name);
+})
 
 //can set the ID
 
