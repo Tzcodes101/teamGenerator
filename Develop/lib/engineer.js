f82d2228e0eced33adbb8dd5d78fab92ae486ddd
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-10 23:53:13 -0500

    engineer extends employee

--- Develop/lib/engineer.js
+++ Develop/lib/engineer.js
@@ -1,33 +1,12 @@
-//create engineer class with github username
+const Employee = require("./Employee");
 
-//get github
-
-//getRole returns engineer
-
-class Engineer {
+class Engineer extends Employee {
     constructor(name, id, email, ghUsername) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.ghUsername = ghUsername;
+       super(name, id, email);
+       this.ghUsername = ghUsername;
     }
 
     //be able to:
-
-    //get name
-    getName() {
-        return this.name;
-    }
-
-    //get id
-    getId() {
-        return this.id;
-    }
-
-    //get email
-    getEmail() {
-        return this.email;
-    }
 
     getGhUserName() {
         return this.ghUsername;
